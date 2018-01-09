d217f92c8d3350b119619f433c0f7ccd3419512b
Dan Starner <starner.daniel5@gmail.com>
2018-01-09 00:05:22 -0500

    Basic Joystick drive added and documented

--- src/org/usfirst/frc/team5590/robot/commands/JoystickDrive.java
+++ src/org/usfirst/frc/team5590/robot/commands/JoystickDrive.java
@@ -5,37 +5,65 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team5590.robot.Robot;
 
 /**
- * Drives the robot using the joysticks
+ * Drives the robot using the joysticks on and
+ * the Drivetrain Subsystem
  */
 public class JoystickDrive extends Command {
 
+	/*
+	 * Initialize the command in Java. This command doesn't
+	 * take any arguments, so its a basic constructor.
+	 */
 	public JoystickDrive() {
-		// This will override any commands that use the drivetrain
+		
+		// This will override any commands that use the Drivetrain
 		requires(Robot.drivetrain);
 	}
 
-	// Called just before this Command runs the first time
+	/*
+	 *  Called just before this Command runs the first time
+	 *  
+	 *  Start the match and make sure we are stopped.
+	 */
 	protected void initialize() {
 		Robot.drivetrain.stop();
 	}
 
-	// Called repeatedly when this Command is scheduled to run
+	/*
+	 * Executes on a loop every 20ms until the command's
+	 * `isFinished` evaluates as true. 
+	 * 
+	 * Seeing as it always returns false, this will 
+	 * always run, meaning the robot drives using 
+	 * the joystick positions.
+	 */
 	protected void execute() {
 		Robot.drivetrain.joystickSpeed();
 	}
-
-	// Make this return true when this Command no longer needs to run execute()
+	
+	/*
+	 * Required for a Command. Since driving will never be
+	 * finished, just always return false. 
+	 */
 	protected boolean isFinished() {
 		return false;
 	}
 
-	// Called once after isFinished returns true
+	/*
+	 *  Called once after isFinished returns true. 
+	 *  Stop the robot if something happens
+	 */
 	protected void end() {
-
+		Robot.drivetrain.stop();
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
+	/*
+	 *  Called when another command which requires one or 
+	 *  more of the same subsystems is scheduled to run
+	 *  
+	 *  Stop the robot if something happens
+	 */
 	protected void interrupted() {
+		Robot.drivetrain.stop();
 	}
 }
--- src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5590.robot.subsystems;
 
 import org.usfirst.frc.team5590.robot.Robot;
+import org.usfirst.frc.team5590.robot.RobotMap;
 import org.usfirst.frc.team5590.robot.commands.JoystickDrive;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
@@ -14,16 +15,6 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 public class Drivetrain extends Subsystem {
 
 	/**
-	 * Port Mappings to the RoboRio
-	 */
-	// Drivetrain motors
-	private static final int LEFT_CONTROLLER_PWM = 1;
-	private static final int RIGHT_CONTROLLER_PWM = 0;
-
-	// Drivetrain
-	private static final int GYROSCOPE_ANALOG_PORT = 1;
-
-	/**
 	 * Constants
 	 */
 	// Driving speeds
@@ -33,27 +24,37 @@ public class Drivetrain extends Subsystem {
 	// Gyroscope metrics
 	private static final double GYRO_SENSITIVITY = .03;
 
-	// Joystick controller deadzones
+	/* Joystick controller deadzones
+	 * Since the controllers are never really at 0, we say anything +/-
+	 * this value from 0 will be considered stopped.
+	 */
 	private static final double DEADZONE_TOLERANCE_LEFT = .15;
-	private static final double DEADZONE_TOLERANCE_RIGHT = .18;
+	private static final double DEADZONE_TOLERANCE_RIGHT = .15;
+	private static final double DEADZONE_TRIGGER_INVERT = .5;
 
 	/**
 	 * Motors and attachments
 	 */
 	private RobotDrive robotDrive; // Allows for us to drive like a tank
-	public Gyro gyro; // Allows for us to measure turn degree
+	public Gyro gyro;              // Allows for us to measure turn degree
 
 	/**
 	 * Constructor for the Drivetrain
 	 */
 	public Drivetrain() {
-		// Create the new Robot Drive system
-		robotDrive = new RobotDrive(LEFT_CONTROLLER_PWM, RIGHT_CONTROLLER_PWM);
+		// Create the new Robot Drive system, using the port mappings
+		// provided in the RobotMap, so we control the right motors.
+		robotDrive = new RobotDrive(
+				RobotMap.DRIVETRAIN_LEFT_CONTROLLER_PWM,
+				RobotMap.DRIVETRAIN_RIGHT_CONTROLLER_PWM
+		);
+		// Disable the Motor Safety Feature
+		// https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599705-using-the-motor-safety-feature
 		robotDrive.setSafetyEnabled(false);
 		robotDrive.setExpiration(.1);
 
-		// Create the Gyroscope
-		gyro = new AnalogGyro(GYROSCOPE_ANALOG_PORT);
+		// Create the Gyroscope, reset the value, and calibrate it
+		gyro = new AnalogGyro(RobotMap.DRIVETRAIN_GYROSCOPE_ANALOG_PORT);
 		gyro.reset();
 		gyro.calibrate();
 		// distanceSensor = new AnalogInput(DISTANCE_SENSOR_INPUT);
@@ -69,8 +70,8 @@ public class Drivetrain extends Subsystem {
 	}
 
 	/**
-	 * This method will set the speed of the motors in the drivetrain to whatever
-	 * *speed* is.
+	 * This method will set the speed of the motors in the drivetrain 
+	 * to whatever `speed` is.
 	 */
 	public void setSpeed(double leftSpeed, double rightSpeed) {
 		robotDrive.tankDrive(leftSpeed, rightSpeed);
@@ -84,19 +85,24 @@ public class Drivetrain extends Subsystem {
 	}
 
 	/**
-	 * This method will set the speed of the drivetrain motors according to the
-	 * joystick controllers.
+	 * This method will set the speed of the Drivetrain motors according
+	 * to the joystick controllers.
 	 */
 	public void joystickSpeed() {
 
-		double left, right;
+		double left, right;  // Initialize the variables for the left and right speed
 
-		if (Robot.oi.xboxController.getRightTrigger() < .5) {
-			// Standard drive
+		/* 
+		 * Based on if the right bumper trigger is pressed, switch which
+		 * side of the robot is either the front or back, making driving easier.
+		 */
+		if (Robot.oi.xboxController.getRightTrigger() < DEADZONE_TRIGGER_INVERT) {
+			// Standard drive using the two thumb knobs
 			left = -1 * Robot.oi.xboxController.getLeftStickY();
 			right = -1 * Robot.oi.xboxController.getRightStickY();
 		} else {
 			// Invert if the robot is facing with the back forward
+			// using the two thumb knobs
 			left = Robot.oi.xboxController.getRightStickY();
 			right = Robot.oi.xboxController.getLeftStickY();
 		}
@@ -105,19 +111,16 @@ public class Drivetrain extends Subsystem {
 		double validLeft = ensureDeadzone(this.ensureRange(left, MIN_SPEED, MAX_SPEED), DEADZONE_TOLERANCE_LEFT);
 		double validRight = ensureDeadzone(this.ensureRange(right, MIN_SPEED, MAX_SPEED), DEADZONE_TOLERANCE_RIGHT);
 
-		// Set the robot motors
+		// Set the robot motors to the deadzoned, ranged, values.
 		robotDrive.tankDrive(validLeft, validRight);
 	}
 
 	/**
 	 * Ensure that a value is within a range
 	 * 
-	 * @param value:
-	 *            the raw value
-	 * @param min:
-	 *            the min value allowed
-	 * @param max:
-	 *            the max value allowed
+	 * @param value: the raw value
+	 * @param min: the min value allowed
+	 * @param max: the max value allowed
 	 * @return: value bounded within [min, max]
 	 */
 	private double ensureRange(double value, double min, double max) {
@@ -127,10 +130,8 @@ public class Drivetrain extends Subsystem {
 	/**
 	 * Sets speed to 0 if its within the tolerance
 	 * 
-	 * @param speed:
-	 *            The raw speed value
-	 * @param tolerance:
-	 *            The tolerance above and below 0 to allow
+	 * @param speed: The raw speed value
+	 * @param tolerance: The tolerance above and below 0 to allow
 	 * @return: Either 0 or the raw speed
 	 */
 	private double ensureDeadzone(double speed, double tolerance) {
